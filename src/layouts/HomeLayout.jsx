8c38bf76c0dc09aa3c4401a6cef73d81d99f20f9
ShakirMahmud <shakirmahmud50@gmail.com>
2024-11-17 21:17:27 +0600

    first commit

---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <header className="w-11/12 mx-auto my-3">
+                <NavBar></NavBar>
+            </header>
+            <main className="w-11/12 mx-auto my-6"><Outlet/></main>
+            <footer></footer>
+        </div>
+    );
+};
+
+export default HomeLayout;
